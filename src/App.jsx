de839a9c58e604997a5cf6ba3aaeaaa6d65ab388
@Chandru18 <chandrusivam18@gmail.com>
2024-07-13 14:57:27 +0530

    file name change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Profile } from "./components/Profile";
 import { Footer } from "./components/Footer";
 
 import './index.css';
-import "./css/Header.css";
+import "./css/header.css";
 import "./css/profile.css";
 import "./css/footer.css";
 function App() {
